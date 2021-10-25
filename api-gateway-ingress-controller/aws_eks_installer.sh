1ddc154eee7e0da8c2ff4f5e87c0f74269b11a13
Li (Boris) Ding <ding@brickdoc.com>
2021-10-25 18:13:06 +0800

    feat: use nginx ingress controller

--- api-gateway-ingress-controller/aws_eks_installer.sh
+++ api-gateway-ingress-controller/aws_eks_installer.sh
@@ -1,19 +1,7 @@
 #!/usr/bin/env bash
 
-ADMIN_KEY=$(openssl rand -hex 32 2>&1)
-VIEWER_KEY=$(openssl rand -hex 32 2>&1)
-echo " ADMIN_KEY is: \`${ADMIN_KEY}\` \nVIEWER_KEY is: \`${VIEWER_KEY}\`"
-
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo add apisix https://charts.apiseven.com
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
-# Apache APISIX as the proxy plane of apisix-ingress-controller, should be deployed in advance.
-helm install apisix apisix/apisix --namespace kube-system --set admin.allow.ipList="{0.0.0.0/0}" \
---set admin.credentials.admin="${ADMIN_KEY}" \
---set admin.credentials.viewer="${VIEWER_KEY}"
 
-helm install apisix-ingress-controller apisix/apisix-ingress-controller  \
-  --set config.apisix.baseURL=http://apisix-admin:9180/apisix/admin \
-  --set config.apisix.adminKey=${ADMIN_KEY} \
-  --set image.tag="1.3.0"
-  --namespace kube-system
+
+helm upgrade --install nginx-ingress ingress-nginx/ingress-nginx -f ./value.yaml --namespace kube-system
